90de0232e3996620903f30b2e98dbdd80581c093
Ivan Joaquim <pichotweb@gmail.com>
2012-07-25 21:51:51 -0300

    Adding a empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTheSecond::Application.config.secret_token = '2c7049f5c0a85de66809289fcf265a40bb8ebab8f2c8d58bd88b7e6b2f5796473aa5d154bd61873f4d89d231d2e7b84b906726729a2c4c32b2652ae61a59f43f'
